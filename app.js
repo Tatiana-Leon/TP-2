88bbdf7a8cbc7acce6e3d16e9219bb9293656e05
Tatiana-Leon <tatianabernalleon@Tatianas-MacBook-Air.local>
2023-06-26 19:41:17 -0400

    test -javascript

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 // API à utiliser pour le travail: https://randomuser.me/api/?results=5
+
+const container = document.getElementById('container');
+
+window.addEventListener('load', () => {
+  fetch('https://randomuser.me/api/?results=5')
+    .then(response => response.json())
+    .then(data => {
+      if (data.results && data.results.length > 0) {
+        data.results.forEach(({ picture, name, email }) => {
+          const ficheDiv = document.createElement('div');
+          ficheDiv.className = 'fiche';
+
+          ficheDiv.innerHTML = `
+            <img class="photo" src="${picture.large}">
+            <p class="nom">Nom: ${name.first} ${name.last}</p>
+            <p class="courriel">Courriel: ${email}</p>
+          `;
+
+          container.appendChild(ficheDiv);
+        });
+      } else {
+        console.log('Aucune donnée de fiche disponible.');
+      }
+    })
+    
+});
